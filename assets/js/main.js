b40718a95b9c20116707c9f4b9c4524c7b97dedb
Sergio Velasquez <sy.velasquez10@uniandes.edu.co>
2018-07-10 14:30:32 -0500

    cambio de tiempo a español

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,10 +23,10 @@
 
      jQuery('#clock').countdown('2018/08/30 8:30',function(event){
       var $this=jQuery(this).html(event.strftime(''
-      +'<div class="time-entry days"><span>%-D</span> Days</div> '
-      +'<div class="time-entry hours"><span>%H</span> Hours</div> '
-      +'<div class="time-entry minutes"><span>%M</span> Minutes</div> '
-      +'<div class="time-entry seconds"><span>%S</span> Seconds</div> '));
+      +'<div class="time-entry days"><span>%-D</span> Días</div> '
+      +'<div class="time-entry hours"><span>%H</span> Horas</div> '
+      +'<div class="time-entry minutes"><span>%M</span> Minutos</div> '
+      +'<div class="time-entry seconds"><span>%S</span> Segundos</div> '));
     });
 
     /* slicknav mobile menu active  */
